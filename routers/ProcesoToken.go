db16b28f937f8f95d635bfef98a574ddcd91de23
Carlos Cardona <chcardonat@gmail.com>
2020-09-12 17:53:45 -0500

    Procesar Token

---
+++ routers/ProcesoToken.go
@@ -0,0 +1,42 @@
+package routers
+
+import (
+	"errors"
+	"github.com/chcardonat/twittor/bd"
+	"github.com/chcardonat/twittor/models"
+	jwt "github.com/dgrijalva/jwt-go"
+	"strings"
+)
+
+//Email valor de Email usado en todos los Endpoints
+var Email string
+
+//IDUsuario es el ID devuelto del modelo, que se usará en todos los Endpoints
+var IDUsuario string
+
+//Proceso Token para extraer sus valores
+func ProcesoToken (tk string)(*models.Claim, bool, string, error){
+	miClave := []byte("MastersDelDesarrollo_grupodeFacebook")
+	claims := &models.Claim{}
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error){
+		return miClave, nil
+	})
+	if err == nil{
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true{
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token Inválido")
+	}
+	return claims, false, string(""), err
+}
