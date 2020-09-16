4275896e302bfd662ab422ae9f8dcf5fa172c323
Carlos Cardona <chcardonat@gmail.com>
2020-09-16 00:05:12 -0500

    Registro de usuarios conectado backend y frontend

--- twittor-react-client/src/components/SignUpForm/SignUpForm.js
+++ twittor-react-client/src/components/SignUpForm/SignUpForm.js
@@ -1,28 +1,61 @@
 import React, { useState } from "react";
-import "./SignUpForm.scss";
 import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import { values, size } from "lodash";
 import { toast } from "react-toastify";
+import { isEmailValid } from "../../utils/validations";
+import { signUpApi } from "../../api/auth";
+
+import "./SignUpForm.scss";
 
 export default function SignUpForm(props) {
   const { setShowModal } = props;
   const [formData, setFormData] = useState(initialFormValue());
+  const [signUpLoading, setSignUpLoading] = useState(false);
 
   const onSubmit = (e) => {
     e.preventDefault();
-    setShowModal(false);
-    console.log(formData);
+
     let validCount = 0;
     values(formData).some((value) => {
       value && validCount++;
       return null;
     });
-    console.log({ validCount });
+
+    if (validCount !== size(formData)) {
+      toast.warning("Completa todos los campos del formulario");
+    } else {
+      if (!isEmailValid(formData.email)) {
+        toast.warning("Email invalido");
+      } else if (formData.password !== formData.repeatPassword) {
+        toast.warning("Las contraseñas tienen que ser iguales");
+      } else if (size(formData.password) < 6) {
+        toast.warning("La contraseña tiene que tener al menos 6 caracteres");
+      } else {
+        setSignUpLoading(true);
+        signUpApi(formData)
+          .then((response) => {
+            if (response.code) {
+              toast.warning(response.message);
+            } else {
+              toast.success("El registro ha sido correcto");
+              setShowModal(false);
+              setFormData(initialFormValue());
+            }
+          })
+          .catch(() => {
+            toast.error("Error del servidor, inténtelo más tarde!");
+          })
+          .finally(() => {
+            setSignUpLoading(false);
+          });
+      }
+    }
   };
 
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
   return (
     <div className="sign-up-form">
       <h2>Crea tu cuenta</h2>
@@ -33,24 +66,24 @@ export default function SignUpForm(props) {
               <Form.Control
                 type="text"
                 placeholder="Nombre"
-                defaultValue={formData.nombre}
                 name="nombre"
+                defaultValue={formData.nombre}
               />
             </Col>
             <Col>
               <Form.Control
                 type="text"
-                placeholder="Apellido"
-                defaultValue={formData.apellido}
-                name="apellido"
+                placeholder="Apellidos"
+                name="apellidos"
+                defaultValue={formData.apellidos}
               />
             </Col>
           </Row>
         </Form.Group>
         <Form.Group>
           <Form.Control
-            type="text"
-            placeholder="Correo electrónico"
+            type="email"
+            placeholder="Correo electronico"
             name="email"
             defaultValue={formData.email}
           />
@@ -69,14 +102,15 @@ export default function SignUpForm(props) {
               <Form.Control
                 type="password"
                 placeholder="Repetir contraseña"
-                name="repeatpassword"
-                defaultValue={formData.repeatpassword}
+                name="repeatPassword"
+                defaultValue={formData.repeatPassword}
               />
             </Col>
           </Row>
         </Form.Group>
+
         <Button variant="primary" type="submit">
-          Registrate
+          {!signUpLoading ? "Registrase" : <Spinner animation="border" />}
         </Button>
       </Form>
     </div>
@@ -86,9 +120,9 @@ export default function SignUpForm(props) {
 function initialFormValue() {
   return {
     nombre: "",
-    apellido: "",
+    apellidos: "",
     email: "",
     password: "",
-    repeatpassword: "",
+    repeatPassword: "",
   };
 }
