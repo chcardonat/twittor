a7ff941ac0126508aac682e73445b92017cc6976
Carlos Cardona <chcardonat@gmail.com>
2020-10-10 09:26:42 -0500

    update en usuarios

--- models/usuario.go
+++ models/usuario.go
@@ -17,5 +17,5 @@ type Usuario struct {
 	Banner          string             `bson:"banner" json:"banner,omitempty"`
 	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
 	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
-	SitioWeb        string             `bson:"sitioweb" json:"sitioWeb,omitempty"`
+	SitioWeb        string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
 }
